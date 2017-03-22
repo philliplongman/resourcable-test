3a631fb8b0b93b323ce1f9fc058ea3a4f2cb196e
Phillip Longman <himself@philliplongman.com>
2017-03-22 00:20:33 -0400

    Move Resourceable::Resource class into seperate file for now.

--- app/controllers/concerns/resourceable.rb
+++ app/controllers/concerns/resourceable.rb
@@ -1,38 +1,6 @@
+require 'resource'
+
 module Resourceable
-
-  class Resource
-    attr_reader :name, :permitted_columns
-
-    def initialize(model)
-      if model.is_a? Hash
-        @name = model.keys.first.to_s.downcase.singularize
-        @permitted_columns = model.values.first
-      else
-        @name = model.to_s.downcase.singularize
-        @permitted_columns = nil
-      end
-    end
-
-    def to_s
-      name
-    end
-
-    def to_sym
-      name.to_sym
-    end
-
-    def pluralize
-      name.pluralize
-    end
-
-    def constantize
-      name.capitalize.constantize
-    end
-
-    def singleton?
-      false
-    end
-  end
 
   private
 
