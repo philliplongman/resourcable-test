759ee9761abb89025d630aae1e01f1f5cd0c52c4
Phillip Longman <himself@philliplongman.com>
2017-03-24 20:31:39 -0400

    Remove unnecessary specs.

--- spec/models/user_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe User, type: :model do
-  it { should have_one :profile }
-  it { should have_many :posts }
-  it { should have_many(:comments).through(:posts) }
-end
